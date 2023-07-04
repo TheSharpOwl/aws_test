f5781b4788dfeed905ef2419064851d802387c2c
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2023-07-04 14:34:57 +0300

    Finish the example

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # aws_test
 Test for aws sdk C++ bug saving files from Aws::StringStream
+
+## How to run:
+
+- Inside the project directory:
+```bash
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE="[path to vcpkg]/vcpkg/scripts/buildsystems/vcpkg.cmake" ..
+```
+
+- docker image command (download and run on port 9000):
+
+```bash
+docker run -p 9000:9000 --name minio  -e "MINIO_ACCESS_KEY=minioadmin" -e "MINIO_SECRET_KEY=miniokey" -v E:/Work/aws/data:/data  minio/minio server /data
+```
--- main.cpp
+++ main.cpp
@@ -2,29 +2,60 @@
 #include <aws/core/auth/AWSCredentialsProvider.h>
 #include <aws/s3/S3Client.h>
 #include <aws/s3/model/PutObjectRequest.h>
+#include "aws/s3/model/CreateBucketConfiguration.h"
+#include "aws/s3/model/CreateBucketRequest.h"
 #include <iostream>
 
 int main() {
+    const char* rawJsonExample = "{\n}\n";
+
     Aws::SDKOptions options;
     Aws::InitAPI(options);
 
     Aws::Client::ClientConfiguration clientConfig;
-    clientConfig.endpointOverride = Aws::String("S3-API endpoint");
+    clientConfig.endpointOverride = Aws::String("127.0.0.1:9000");
     clientConfig.scheme = Aws::Http::Scheme::HTTP;
+    clientConfig.verifySSL = false;
+
     Aws::S3::S3Client client(Aws::Auth::AWSCredentials("minioadmin", "miniokey"), clientConfig,
                              Aws::Client::AWSAuthV4Signer::PayloadSigningPolicy::Never, false);
 
+    auto listBucketsResult = client.ListBuckets();
+    if (listBucketsResult.IsSuccess()) {
+        std::cout << "FINE\n";
+    } else {
+        std::cout << "NOT OK\n";
+        return -1;
+    }
+
+    Aws::String bucketName = "anything";
+    Aws::String fileName = "myFile.json";
+
+    Aws::S3::Model::CreateBucketRequest request;
+    request.SetBucket(Aws::String(bucketName));
+    auto outcome = client.CreateBucket(request);
+
+    if(!outcome.IsSuccess())
+    {
+        if(outcome.GetError().GetExceptionName() != "BucketAlreadyOwnedByYou")
+        {
+            std::cout << "FAILED TO CREATE A BUCKET\n";
+            std::cout << outcome.GetError().GetExceptionName() << " " << outcome.GetError().GetMessage() << "\n";
+            return -1;
+        }
+    }
+
     Aws::S3::Model::PutObjectRequest putObjectRequest;
-    putObjectRequest.SetContentType("image/jpeg");
-    Aws::String bucketName = "myBucketName";
-    Aws::String fileName = "myFile.jpg";
+    // no set content type because it's json file
     putObjectRequest.SetBucket(bucketName.c_str());
     putObjectRequest.WithKey(fileName.c_str());
 
     auto inputData = Aws::MakeShared<Aws::StringStream>(
             "PutObjectInputStream",
             fileName.c_str(),
-            std::stringstream::in | std::stringstream::binary);
+            std::stringstream::in | std::stringstream::binary | std::stringstream::out);
+
+    inputData->write(rawJsonExample, std::string(rawJsonExample).size());
 
     putObjectRequest.SetBody(inputData);
     auto result = client.PutObject(putObjectRequest);
@@ -33,8 +64,6 @@ int main() {
     } else {
         std::cout << "PutObject error: " << result.GetError().GetExceptionName() << " " << result.GetError().GetMessage() << std::endl;
     }
-
-    Aws::ShutdownAPI(options);
 
     return 0;
 }
